6d50997be68f88275aab0036c4d78caf5072ac09
Sanghyuk Park <dropthegit@gmail.com>
2023-10-31 10:04:13 +0900

    PR1

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,6 +3,7 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
         System.out.println("main brach 생성");
+        System.out.println("1번 PR, e/1");
 
     }
 }
